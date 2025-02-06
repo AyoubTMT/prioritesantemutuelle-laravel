05b51d0ede69e51639e6d6b3c49ce50700bf6fa6
MOHAMED AYOUB TAJMOUT <m.tajmout@eca-assurances.com>
2025-02-06 15:43:04 +0100

    contact mail

--- resources/views/mails/homeContact.blade.php
+++ resources/views/mails/homeContact.blade.php
@@ -7,10 +7,10 @@
 </head>
 <body>
     <h2>Nouveau message de contact</h2>
-    <p><strong>Prénom:</strong> {{ $contactData['firstName'] }}</p>
-    <p><strong>Nom:</strong> {{ $contactData['lastName'] }}</p>
+    <p><strong>Prénom:</strong> {{ $contactData['prenom'] }}</p>
+    <p><strong>Nom:</strong> {{ $contactData['nom'] }}</p>
     <p><strong>Email:</strong> {{ $contactData['email'] }}</p>
-    <p><strong>Numéro de téléphone:</strong> {{ $contactData['phone'] }}</p>
+    <p><strong>Numéro de téléphone:</strong> {{ $contactData['telephone'] }}</p>
     <p><strong>Message:</strong> {{ $contactData['message'] }}</p>
 </body>
 </html>
